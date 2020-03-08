f6d31a6740d17fc756370d8a0da12790deef1f29
Justin <jbattles393@gmail.com>
2020-03-08 17:43:24 -0400

    Update fizzbuzz.s

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -1,23 +1,3 @@
-;#include <stdio.h>
-;
-;/**
-; * (4 points)
-; */
-;int main(void)
-;{
-;  int i = 1;
-;  
-;  while (i <= 100) {
-;    if (i % 5 == 0 && i % 3 == 0) printf("FizzBuzz\n");
-;    else if (i % 3 == 0) printf("Fizz\n");
-;    else if (i % 5 == 0) printf("Buzz\n");
-;    else printf("%d\n", i);
-;    i++;
-;  }
-;
-;  return 0;
-;}
-
 %include "lib.s"
 %include "syscalls.h"
 
