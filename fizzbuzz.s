f6c8702c230d3a7c15f9f2a265edf9d6e72a432b
Justin <jbattles393@gmail.com>
2020-03-08 17:43:11 -0400

    Add files via upload

---
+++ fizzbuzz.s
@@ -0,0 +1,90 @@
+;#include <stdio.h>
+;
+;/**
+; * (4 points)
+; */
+;int main(void)
+;{
+;  int i = 1;
+;  
+;  while (i <= 100) {
+;    if (i % 5 == 0 && i % 3 == 0) printf("FizzBuzz\n");
+;    else if (i % 3 == 0) printf("Fizz\n");
+;    else if (i % 5 == 0) printf("Buzz\n");
+;    else printf("%d\n", i);
+;    i++;
+;  }
+;
+;  return 0;
+;}
+
+%include "lib.s"
+%include "syscalls.h"
+
+extern printnum, puts, putc
+
+SECTION .data
+	fizzbuzz: db `fizzbuzz\n`, 0
+	fizz: db `fizz\n`, 0
+	buzz: db `buzz\n`, 0
+
+SECTION .text
+
+global _start
+_start:
+	mov r14, 1 
+.while:
+	cmp r14, 100
+	jg .endwhile
+	mov rax, r14
+	mov r15, 3
+	cqo
+	div r15
+	mov r11, rdx    
+
+	mov rax, r14
+	mov r15, 5
+	cqo
+	div r15
+	mov r13, rdx   
+
+	cmp r11, 0
+	je .fizz
+	cmp r13, 0
+	je .buzz
+	jmp .nothing
+
+.fizz:
+	cmp r13, 0
+	je .fizzbuzz
+	mov rsi, fizz
+	call puts
+	jmp .continue
+
+.buzz:
+	mov rsi, buzz
+	call puts
+	jmp .continue
+
+.nothing:
+	mov rax, r14
+	call printnum
+	mov al, BYTE`\n`
+	call putc
+	jmp .continue 
+
+.fizzbuzz:
+	mov rsi, fizzbuzz
+	call puts
+	jmp .continue	
+	
+.continue:
+	inc r14
+	jmp .while
+
+	
+.endwhile:
+	mov rax, 60
+	mov rdi, 0
+	syscall	
+	
